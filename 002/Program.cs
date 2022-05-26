cccb92d538dc8db9b03ff514f490ccc129ac18e9
Ekaterina <Kuteynikova.ekaterina@mail.ru>
2022-05-26 17:18:21 +0300

    Home_002

---
+++ 002/Program.cs
@@ -0,0 +1,5 @@
+﻿double Distanse(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    return Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2)),4);
+}
+Console.Write(Distanse(3, 6, 8, 2, 1, -7));
