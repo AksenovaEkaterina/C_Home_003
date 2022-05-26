935b6485b496c288c7e1e0d1e79ffe7cc6c95e89
Ekaterina <Kuteynikova.ekaterina@mail.ru>
2022-05-26 17:16:17 +0300

    Home_001

---
+++ 001/Program.cs
@@ -0,0 +1,16 @@
+﻿void Palindrom (int n)
+{
+    int num1 = n/1000;
+    int num2 = n%10 *10 + n%100 / 10;
+    if (num1 == num2)
+    {
+        Console.WriteLine ($"{n} -> yes");
+    }
+    else
+    {
+        Console.WriteLine ($"{n} -> no");
+    }
+}
+Palindrom (89698);
+Palindrom (78342);
+Palindrom (45654);
